19ed7f6c332d7bd62b0db1ef2c1b897637054733
vijay-durga <svijay781783@gmail.com>
2023-01-09 11:10:14 +0000

    Nxt-Trendz-Authentication-2

---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-cont">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
